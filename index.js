aabbd56c5d85738b0cab6ffe6b23c34fff588e81
Adiel <adiela@wix.com>
2016-12-29 12:38:25 +0200

    passing nodeSassConfiguration, removing clean-css dependency - just use node-sass options to minify etc... making the script smaller.

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 var fs = require('fs');
 var glob = require('glob-fs')({ gitignore: true });
 var sass = require('node-sass');
-var CleanCSS = require('clean-css');
 
 var wrapResultWithInjectScript = function (css) {
-    var minified = new CleanCSS().minify(css).styles;
+    css = css.toString().replace(/\n/g, "\\n");
     return '' +
         '(function(){' +
+            'var st = "' + css + '";' +
             'var h = document.getElementsByTagName("head")[0];' +
             'var s = document.createElement("style");' +
             's.setAttribute("type", "text/css");' +
             'if (s.styleSheet) {' + // IE
-                's.styleSheet.cssText = "' + minified + '";' +
+                's.styleSheet.cssText = st;' +
             '} else {' + // the world
-                's.appendChild(document.createTextNode("'+ minified +'"));' +
+                's.appendChild(document.createTextNode(st));' +
             '}' +
             'h.appendChild(s);' +
         '})();'
 };
 
-module.exports = function (pattern) {
+module.exports = function (pattern, nodeSassConfiguration) {
+    nodeSassConfiguration = nodeSassConfiguration || {};
     var files = glob.readdirSync(pattern);
     files.forEach(function(file){
-        sass.render({
+        sass.render(Object.assign({
             file: file
-        }, function(err, result) {
-            fs.writeFile(file + '.js', wrapResultWithInjectScript(result.css))
+        }, nodeSassConfiguration), function(err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                fs.writeFile(file + '.js', wrapResultWithInjectScript(result.css))
+            }
         });
     })
 };
